cc72c262ea25437ec0d7b7c01cc5a3b4f1562092
Pedro Rosário <pedro.rosario@neomind.com.br>
2022-07-28 00:48:59 -0300

    Correções: Criação dos serviços: FraudDetectorService, EmailService and LogService.
    Refatoração da classe NewOrderMain para atender aos novos serviços.
    
    Controle de processamento de registro pelo kafka.

--- src/main/java/com/ecommerce/EmailService.java
+++ src/main/java/com/ecommerce/EmailService.java
@@ -1,26 +1,25 @@
 package com.ecommerce;
 
-import java.util.UUID;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-
-public class FraudDetectorService {
+public class EmailService
+{
 
     public static void main(String[] args) {
         var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
         while(true) {
             var records = consumer.poll(Duration.ofMillis(100));
             if (!records.isEmpty()) {
                 System.out.println("Encontrei " + records.count() + " registros");
                 for (var record : records) {
                     System.out.println("------------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
+                    System.out.println("Send email for you");
                     System.out.println(record.key());
                     System.out.println(record.value());
                     System.out.println(record.partition());
@@ -42,9 +41,7 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "_" + UUID.randomUUID().toString());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
         return properties;
     }
 }
--- src/main/java/com/ecommerce/NewOrderMain.java
+++ src/main/java/com/ecommerce/NewOrderMain.java
@@ -1,5 +1,7 @@
 package com.ecommerce;
 
+import java.util.UUID;
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,15 +14,25 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
+
+        var key = UUID.randomUUID().toString();
+
         var value = "132123,67523,1234";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        var newOrder = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+
+        Callback callback = (data, ex) -> {
             if(ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset "  + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+            System.out.println("Send successful - " + data.topic() + ":::partition " + data.partition() + "/ offset "  + data.offset() + "/ timestamp " + data.timestamp());
+        };
+
+        var emailText = "Great ! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, emailText);
+
+        producer.send(newOrder, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
